0619ecbd9b94b49eb3989918b90be93797ee949c
Simad9 <wijdanbita@gmail.com>
2023-11-29 00:47:52 +0700

    detail saat login kelar

--- detail.php
+++ detail.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include "koneksi.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,11 +11,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Detail</title>
 
+  <!-- Tamplate -->
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
+  <link href="css/style.css" rel="stylesheet" />
+  <link href="css/responsive.css" rel="stylesheet" />
+
   <!-- BOOTSTRAP -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
   </script>
   <!-- ICONS BOOTSTRAP -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
@@ -23,58 +31,65 @@
   <nav class="navbar bg-grey py-2 px-5 shadow-lg d-flex align-items-center">
     <h3 class="navbar-brand text-putih">Dapur Kita</h3>
     <div class="d-flex gap-2">
-      <a class="text-decoration-none text-light nav-item" href="#">Home</a>
-      <a class="text-decoration-none text-light nav-item" href="#">Menu</a>
-      <a class="text-decoration-none text-light nav-item" href="#">About</a>
-      <a class="text-decoration-none text-light nav-item" href="#">News</a>
+      <a class="text-decoration-none text-light nav-item" href="homepage.php">Home</a>
+      <a class="text-decoration-none text-light nav-item" href="menuSudahLogin.php">Menu</a>
     </div>
     <div class="d-flex justify-content-center align-items-center gap-3">
       <i class="bi bi-search fs-5 text-putih"></i>
-      <img src="img/profile.jpeg" alt="" class="profilePicture">
+      <img src="img/profile/<?= $_SESSION['profilePicture'] ?>" alt="" class="profilePicture">
     </div>
   </nav>
   <!-- END NAVBAR -->
 
   <!-- Main Section -->
-  <section class="container d-flex flex-column gap-4 pt-4">
-    <img src="./img/cendol.webp" alt="" class="gambar-resep rounded-4 shadow-lg">
+  <?php
+  $id_resep = $_GET["id_resep"];
+  $id_user = $_SESSION['id_user'];
+
+  $query = "SELECT * FROM users u, resep r WHERE u.id_user = r.id_user AND r.id_resep = $id_resep AND r.id_user = $id_user";
+
+
+  $hasil = mysqli_query($konek, $query);
+
+  if (mysqli_num_rows($hasil) === 0) {
+    // Nanti dirapikan lagi buat control handlingnya
+    echo "<h1>Data tidak ada / Terjadi Kesalahan</h1>";
+    die;
+  }
+  $data = mysqli_fetch_array($hasil);
+  ?>
+  <section class="container d-flex flex-column gap-4 pt-4 pb-5">
+    <img src="./img/resep/<?= $data['foto'] ?>" alt="Foto Resep" class="gambar-resep rounded-4 shadow-lg">
     <div class="kotak shadow">
-      <h4>Cendol Tepung Beras Daun Suji</h4>
-      <p class="m-0">By. Wijdan</p>
+      <h4><?= $data['judul'] ?></h4>
+      <p class="m-0">By. <?= $data['namaLengkap'] ?></p>
+      <p class="m-0 pt-2"> <?= $data['deskripsi'] ?></p>
     </div>
     <div class="kotak shadow d-flex flex-column gap-1">
       <h5>Bahan-bahan</h5>
       <div class="garis"></div>
       <p class="m-0">
-        60 gr tepung beras (rosebrand) <br>
-        20 gr tepung tapioka (liauw liong pit) <br>
-        Sedikit garam halus <br>
-        375 ml Air perasan daun suji+pandan <br>
-        Air es + es batu
+        <?= $data['bahan_bahan'] ?>
       </p>
     </div>
     <div class="kotak shadow d-flex flex-column gap-1">
-      <h5>Bahan-bahan</h5>
+      <h5>Cara Pembuatan</h5>
       <div class="garis"></div>
       <p class="m-0">
-        60 gr tepung beras (rosebrand) <br>
-        20 gr tepung tapioka (liauw liong pit) <br>
-        Sedikit garam halus <br>
-        375 ml Air perasan daun suji+pandan <br>
-        Air es + es batu
+        <?= $data['cara_pembuatan'] ?>
       </p>
     </div>
     <div class="kotak shadow">
       <h5>Ditulis Oleh</h5>
       <div class="garis"></div>
       <div class="d-flex gap-3 justify-content-start align-items-center mb-2">
-        <img src="img/profile.jpeg" alt="" class="profileDitulis">
+        <img src="img/profile/<?= $data['profilePicture'] ?>" alt="" class="profileDitulis">
         <div>
-          <h6 class="fs-5 mb-0">Wijdan</h6>
-          <p class="m-0">pada 00 September 2023</p>
+          <h6 class="fs-5 mb-0"><?= $data['namaLengkap'] ?></h6>
+          <p class="m-0">pada <?= date('d F Y', strtotime($data['tgl_pembuatan'])) ?> </p>
         </div>
       </div>
-      <p class="my-0"> Diskripsi Orang</p>
+      <p class="my-0"> <?= $data['headline'] ?></p>
 
     </div>
     <div class="kotak shadow">
@@ -84,23 +99,24 @@
       </div>
       <div class="garis"></div>
       <div class="d-flex flex-wrap gap-3">
+        <?php
+        $query = "SELECT foto, judul FROM resep WHERE id_user = $id_user";
+        $hasil = mysqli_query($konek, $query);
+        while ($resep = mysqli_fetch_array($hasil)) :
+        ?>
+          <div class="bg-grey border-radius-15px">
+            <img src="img/resep/<?= $resep['foto'] ?>" alt="" class="gambar-card">
+            <p class="my-1 py-1 text-wrap text-center text-putih"><?= $resep['judul'] ?></p>
+          </div>
+        <?php endwhile; ?>
 
-        <div class="bg-grey border-radius-15px">
-          <img src="img/cendol.webp" alt="" class="gambar-card">
-          <p class="my-1 py-1 text-wrap text-center text-putih">Judul Resep</p>
-        </div>
-
-        <div class="bg-grey border-radius-15px">
-          <img src="" alt="" class="gambar-card">
-          <p class="my-1 py-1 text-wrap text-center text-putih">Judul Resep</p>
-        </div>
       </div>
     </div>
   </section>
 
   <!-- FOOTER SECTIOn -->
   <!-- COPAS DARI TAMPLATE -->
-  <!-- <footer class="footer_section">
+  <footer class="footer_section">
     <div class="container">
       <div class="row">
         <div class="col-md-4 footer-col">
@@ -120,7 +136,7 @@
         </div>
         <div class="col-md-4 footer-col">
           <div class="footer_detail">
-            <a href="" class="footer-logo"> Dapur Kita </a>
+            <a href="homepage.php" class="footer-logo"> Dapur Kita </a>
             <p style="text-align: justify">
               Dapur Kita, sumber inspirasi kuliner penuh kreativitas! Temukan
               resep-resep lezat dari berbagai masakan, ikuti tips ahli, dan
@@ -159,7 +175,7 @@
         </p>
       </div>
     </div>
-  </footer> -->
+  </footer>
 
 </body>
 
